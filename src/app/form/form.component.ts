6b5a6b9406062f745933c6fdd96118c4a20ba99f
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-04-12 19:20:34 +0300

    add form-quote

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component, OnInit,Output,Input,EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 
 @Component({
@@ -7,7 +8,13 @@ import { Component, OnInit, } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
-  
+  quote: any;
+  @Output() addQuote = new EventEmitter<Quote>();
+  submitQuote() {
+    this.addQuote.emit(this.newQuote)
+  }
+  newQuote = new Quote(0, "", "",0,0);
+
 
   
   
