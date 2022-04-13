837fd133bf86cb96dd7e1fe2fddc631b6bdfbc8e
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-04-13 19:16:45 +0300

    add quotes-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { FormComponent } from './form/form.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { MainComponent } from './main/main.component';
 import { DetailsComponent } from './QUOTES/src/app/quotes/details/details.component';
+import { LogoComponent } from './logo/logo.component';
+import { QuoteDetailComponent } from './QUOTES/src/app/quotes-folder/quote-detail/quote-detail.component';
+import { QuoteFormComponent } from './QUOTES/src/app/quotes-folder/quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +20,10 @@ import { DetailsComponent } from './QUOTES/src/app/quotes/details/details.compon
     FormComponent,
     QuotesComponent,
     MainComponent,
-    DetailsComponent
+    DetailsComponent,
+    LogoComponent,
+    QuoteDetailComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
