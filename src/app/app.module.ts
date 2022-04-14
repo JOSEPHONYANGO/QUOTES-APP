a68b8ac115d832b0b716d762a7a532c69a1d77c1
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-04-14 04:16:45 +0300

    add directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataComponent } from './data/data.component';
-import { FormComponent } from './form/form.component';
-import { QuotesComponent } from './quotes/quotes.component';
-import { MainComponent } from './main/main.component';
-import { DetailsComponent } from './QUOTES/src/app/quotes/details/details.component';
 import { LogoComponent } from './logo/logo.component';
 import { QuoteDetailComponent } from './QUOTES/src/app/quotes-folder/quote-detail/quote-detail.component';
 import { QuoteFormComponent } from './QUOTES/src/app/quotes-folder/quote-form/quote-form.component';
+import { DateCountPipe } from './date-count.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataComponent,
-    FormComponent,
-    QuotesComponent,
-    MainComponent,
-    DetailsComponent,
     LogoComponent,
     QuoteDetailComponent,
-    QuoteFormComponent
+    QuoteFormComponent,
+    DateCountPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
